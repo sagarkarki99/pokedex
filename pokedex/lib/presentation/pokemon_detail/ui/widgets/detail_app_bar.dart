b08ec67ee9afffaf9147c249c79cd37618c2ed06
sagarkarki99 <sagarkarki184@gmail.com>
2022-04-13 19:10:36 +0545

    ui refactor

--- pokedex/lib/presentation/pokemon_detail/ui/widgets/detail_app_bar.dart
+++ pokedex/lib/presentation/pokemon_detail/ui/widgets/detail_app_bar.dart
@@ -1,10 +1,9 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../ui/ui.dart';
 import '../../cubit/pokemon_detail_cubit.dart';
+import '../../view_model/pokemon_detail_view_model.dart';
 
 class DetailAppBar extends StatelessWidget {
   const DetailAppBar({Key? key}) : super(key: key);
@@ -36,36 +35,48 @@ class DetailAppBar extends StatelessWidget {
               ),
               titlePadding:
                   const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
-              background: Stack(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 12.0),
-                    color: pokemon.getBgColor(),
-                    alignment: Alignment.bottomRight,
-                    child: Hero(
-                      tag: pokemon.id,
-                      child: PokemonImage(
-                        url: pokemon.svgUrl,
-                        height: 125,
-                        width: 136,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 16.0,
-                    top: MediaQuery.of(context).size.height * 0.12,
-                    child: _TitleSection(
-                      title: pokemon.name,
-                      skill: pokemon.type,
-                    ),
-                  ),
-                ],
-              ),
+              background: _BackgroundView(model: pokemon),
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class _BackgroundView extends StatelessWidget {
+  final PokemonDetailViewModel model;
+  const _BackgroundView({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          color: model.getBgColor(),
+          alignment: Alignment.bottomRight,
+          child: Hero(
+            tag: model.id,
+            child: PokemonImage(
+              url: model.svgUrl,
+              height: 125,
+              width: 136,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 16.0,
+          top: MediaQuery.of(context).size.height * 0.12,
+          child: _TitleSection(
+            title: model.name,
+            skill: model.type,
+          ),
+        ),
+      ],
     );
   }
 }
